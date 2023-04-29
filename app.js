366eed31a503a2af376c701567b92bd7529aad9b
Hello Good <codeo@lampstand.ca>
2023-04-29 09:20:56 -0400

    created filter buttons

--- app.js
+++ app.js
@@ -74,12 +74,41 @@ const menu = [
 ];
 
 const sectionCenter = document.querySelector('.section-center');
+const filterBtns = document.querySelectorAll('.filter-btn');
 
+
+
+// load items
 window.addEventListener("DOMContentLoaded", function(){
   // console.log('loaded potats');
-  let displayMenu = menu.map(function(item){
-    // console.log(item);
+  displayMenuItems(menu)
+});
 
+// filter items
+filterBtns.forEach(function(btn){
+  btn.addEventListener('click', function(e){
+    const category = e.currentTarget.dataset.id;
+    const menuCategory = menu.filter(function(menuItem){
+      if (menuItem.category === category) {
+        return menuItem;
+      }
+    });
+    // console.log(menuCategory);
+    if (category === 'all'){
+      displayMenuItems(menu)
+    }
+    else {
+      displayMenuItems(menuCategory)
+    }
+  })
+})
+
+
+
+
+function displayMenuItems(menuItems){
+  let displayMenu = menuItems.map(function(item){
+    // console.log(item);
     return `<article class="menu-item">
     <img src=${item.img} class="photo" alt=${item.title}>
     <div class="item-info">
@@ -95,5 +124,4 @@ window.addEventListener("DOMContentLoaded", function(){
   });
   displayMenu = displayMenu.join("");
   sectionCenter.innerHTML = displayMenu;
-
-});
+};
